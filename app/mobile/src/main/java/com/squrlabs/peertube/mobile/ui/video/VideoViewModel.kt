83fed625a2a9e4640d4865626131e5eafdfb380a
deepanshut041 <deepanshut041@gmail.com>
2020-12-20 01:10:25 +0530

    Updated code and removed unwanted models

--- app/mobile/src/main/java/com/squrlabs/peertube/mobile/ui/video/VideoViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.squrlabs.peertube.mobile.ui.video
-
-import androidx.lifecycle.ViewModel
-import com.squrlabs.peertube.common.service.repository.VideoRepository
-
-class VideoViewModel(private val videoRepository: VideoRepository): ViewModel() {
-
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,3 @@ rootProject.name = "PeerTube"
 enableFeaturePreview("GRADLE_METADATA")
 include(":app")
 include(":common")
-include(":app:mobile")
-include(":app:tv")
